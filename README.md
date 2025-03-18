12ebcabe1daa1b7d1735dd4d45a6cf6d836cc671
JosueJNLui <nlui.josuej@gmail.com>
2025-03-18 20:54:55 -0300

    feat: ingest data from other sources

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Repo to build up my monography project
 ## Useful links
 - https://apisidra.ibge.gov.br/
 - https://dadosabertos.bcb.gov.br/
+- https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/precos-revenda-e-de-distribuicao-combustiveis/serie-historica-do-levantamento-de-precos
+- 
--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,14 +1,36 @@
 import requests
 import json
 from utils.utils import Utils
+# from unicodedata import normalize
+# import re
 
-class BCB:
+# def process_text(raw_text: str):
+    
+#     text = normalize('NFKD', raw_text).encode('ASCII', 'ignore').decode('ASCII')
+#     text = ' '.join(text.strip().split())
+
+#     return re.sub('[^A-Za-z0-9 ]+', '', text)
+
+class API:
 
     def __init__(self) -> None:
 
-        self.url_std = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato={}&dataInicial={}&dataFinal={}"
+        self.url_bcb = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato={}&dataInicial={}&dataFinal={}"
+        self.url_sidra = "https://apisidra.ibge.gov.br/values/{}"
     
-    def get_data(self, code: str, format: str, start_date: str, end_date: str) -> json:
+    def get_bcb_data(self, code: str, format: str, start_date: str, end_date: str) -> json:
+
+        url = self.url_bcb.format(code, format, start_date, end_date)
+
+        return self.get_request(url)
+
+    def get_sidra_data(self, query_parameters: str) -> json:
+
+        url = self.url_sidra.format(query_parameters)
+
+        return self.get_request(url)
+
+    def get_request(self, url: str) -> json:
         """
         Get data from BCB API - Temporal Series System Manager.
 
@@ -22,7 +44,7 @@ class BCB:
             json: The JSON response from the API.
         """
 
-        url = self.url_std.format(code, format, start_date, end_date)
+        # url = self.url_std.format(code, format, start_date, end_date)
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -35,17 +57,37 @@ if __name__ == '__main__':
     start_date = '01/01/1999'
     end_date = '01/01/2025'
 
-    bcb = BCB()
+    api = API()
 
-    codes = {
-        'ipca_monthly': 10844,
-        'selic_daily': 4390,
+    bcb_codes = {
+        'ipca': 10844,
+        'selic': 4390,
         'cambio': 10813,
-        'm2': 1787
+        'm2': 1787,
+        'icc': 4393
     }
 
-    for name, code in codes.items():
+    sidra_query_parameters = {
+        'desocupacao': "t/4093/n1/all/v/4099/p/all/c2/6794"
+    }
 
-        data = bcb.get_data(code, 'json', start_date, end_date)
+
+    # for name, code in bcb_codes.items():
+
+    #     data = api.get_bcb_data(code, 'json', start_date, end_date)
         
-        Utils.save_json(data, '../data', name)
+    #     Utils.save_json(data, '../data', name)
+
+    for name, param in sidra_query_parameters.items():
+
+        data = api.get_sidra_data(param)
+
+        # processed_data = [
+        #     {k: process_text(v) if isinstance(v, str) else v 
+        #      for k, v in d.items()}
+        #      for d in data
+        # ]
+
+
+        
+    Utils.save_json(data, '../data', name)
