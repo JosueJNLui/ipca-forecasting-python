f7286a118c9f1dd7d6a263f6ae0de196ab596c6a
JosueJNLui <nlui.josuej@gmail.com>
2025-03-27 02:02:08 +0000

    feat: refactoring project

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Repo to build up my monography project
 - https://apisidra.ibge.gov.br/
 - https://dadosabertos.bcb.gov.br/
 - https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/precos-revenda-e-de-distribuicao-combustiveis/serie-historica-do-levantamento-de-precos
-- 
+- https://www4.bcb.gov.br/pec/series/port/metadados/mg41Bp.htm
--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -1,42 +1,57 @@
 from ingestion.api import API
-from ingestion.utils import Utils, SecretsManagerSecret
+from ingestion.utils import Utils
+from loguru import logger
+from datetime import datetime
+from ingestion.models import BCBJobParameters, SidraJobParameters
+import fire
+
 
 def main() -> None:
+    start_time = datetime.now()
 
-    # start_date = '01/01/1999'
-    # end_date = '01/01/2025'
+    bcb_params = BCBJobParameters(
+        start_date="01/01/1999",
+        end_date="01/01/2025",
+        format="json",
+        code=10844,
+        table_name="test_table",
+        destination="local_path",
+        s3_path="s3://bucket/path",
+        aws_profile="dev",
+    )
 
-    # api = API()
+    sidra_params = SidraJobParameters(
+        code="t/4093/n1/all/v/4099/p/all/c2/6794",
+        table_name="test_table",
+        destination="local_path",
+        s3_path="s3://bucket/path",
+        aws_profile="dev",
+    )
 
-    # data_sources = {
+    api = API()
 
-    #     'bcb': {
-    #         'ipca': 10844,
-    #         'selic': 4390,
-    #         'cambio': 10813,
-    #         'm2': 1787,
-    #         'icc': 4393
-    #     },
-    #     'sidra': {
-    #         'desocupacao': "t/4093/n1/all/v/4099/p/all/c2/6794"
-    #     }
-    # }
+    data_sources = {
+        "bcb": {"ipca": 10844, "selic": 4390, "cambio": 10813, "m2": 1787, "icc": 4393},
+        "sidra": {"desocupacao": "t/4093/n1/all/v/4099/p/all/c2/6794"},
+    }
 
-    # for source, values in data_sources.items():
+    for source, values in data_sources.items():
+        for name, param in values.items():
+            if source == "bcb":
+                data = api.get_bcb_data(bcb_params)
 
-    #     for name, param in values.items():
-            
-    #         if source == 'bcb':
-    #             data = api.get_bcb_data(param, 'json', start_date, end_date)
-            
-    #         if source == 'sidra':
-    #             data = api.get_sidra_data(param)
-            
-    #         Utils.save_json(data, 'data', name)
+            if source == "sidra":
+                data = api.get_sidra_data(sidra_params)
 
-    client = SecretsManagerSecret('us-east-2', 'dev')
-    print(client.client.list_buckets())
-    
+            logger.info(f"Saving {name.upper()} as JSON file.")
+            # Utils.save_json(data, "data", name)
+
+    end_time = datetime.now()
+    elapsed = (end_time - start_time).total_seconds()
+    logger.info(
+        f"Job completed in {elapsed // 60} minutes and {elapsed % 60:.2f} seconds."
+    )
+
 
 if __name__ == "__main__":
     main()
